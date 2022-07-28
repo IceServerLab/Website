da938e61b0f6dc6221eb4d88e19a08dac59e2394
みにどん <48941476+M1n1don@users.noreply.github.com>
2022-07-28 21:01:09 +0900

    [Add] サーバー参加方法へ飛ばす

--- src/components/carousel/CarouselItem.vue
+++ src/components/carousel/CarouselItem.vue
@@ -3,7 +3,7 @@
     <div class="carousel-container">
       <h2 class="animate__animated animate__fadeInDown"><slot name="title" /></h2>
       <p class="animate__animated animate__fadeInUp"><slot name="content" /></p>
-      <a href="https://minecraft.jp/servers/mc.iceserver.jp" class="btn-get-started animate__animated animate__fadeInUp" target="_blank">Join The Server</a>
+      <a href="#features" class="btn-get-started animate__animated animate__fadeInUp" target="_blank">Join The Server</a>
     </div>
   </div>
 </template>
