5d8f1a37887d219aec3f07862a97bbc4f46e5f43
みにどん <48941476+M1n1don@users.noreply.github.com>
2022-07-28 20:48:18 +0900

    [Removed] 不要なスクリプトを削除

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,3 @@
-<script setup>
-import Breadcrumbs from "@/components/Breadcrumbs.vue"
-</script>
-
 <template>
   <main id="main">
     <img src="https://topchiase24h.com/wp-content/uploads/2021/04/error-404-06.jpg">
